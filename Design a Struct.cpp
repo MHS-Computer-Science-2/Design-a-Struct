06ac62f3f643a690e11ffbd973e78169b2193dc9
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-03-31 10:47:01 -0400

    Add files via upload

---
+++ Design a Struct.cpp
@@ -0,0 +1,16 @@
+// Struct Notes
+#include <iostream>
+using namespace std;
+
+// Create your struct here (Book, Student, or Car)
+
+
+int main()
+{
+    // Create an object, set all the attributes to values of your choosing,
+    // and print all of the attributes neatly formatted
+
+
+    system("pause");
+}
+
